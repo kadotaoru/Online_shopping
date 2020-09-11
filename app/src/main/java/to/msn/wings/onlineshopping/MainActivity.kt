bfb3c2e939f4db1231d7357f27d312dc60605688
kadotaoru <daiyaogata898@gmail.com>
2020-09-11 21:05:59 +0900

    Initial commit

---
+++ app/src/main/java/to/msn/wings/onlineshopping/MainActivity.kt
@@ -0,0 +1,16 @@
+package to.msn.wings.onlineshopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val menuList:MutableList<MutableMap<String, String>> = mutableListOf()
+        
+    }
+}
